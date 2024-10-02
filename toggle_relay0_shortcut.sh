4cf9bb457eb9a9b25236f7af31a37d80ec3824c1
e-myer <amirsamir92@gmail.com>
2024-10-02 23:32:35 +0600

    renamed script directory variable

--- toggle_relay0_shortcut.sh
+++ toggle_relay0_shortcut.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
 # A portion of the following line has been taken from here: https://stackoverflow.com/a/246128
-file_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-flock -n "$file_dir/toggle_relay0.lock" "$file_dir/toggle_relay0.sh"
+flock -n "$script_dir/toggle_relay0.lock" "$script_dir/toggle_relay0.sh"
